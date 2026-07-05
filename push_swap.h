19b069a1277513e9d7bbc4b3fe3c54cdccb0832c
Shiho Waragaya <swaragay@c6r3s4.42tokyo.jp>
2026-07-05 21:17:46 +0900

    0705

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: swaragay <swaragay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 18:27:40 by swaragay          #+#    #+#             */
-/*   Updated: 2026/07/05 21:03:26 by swaragay         ###   ########.fr       */
+/*   Updated: 2026/07/05 21:14:25 by swaragay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,31 @@
 
 #include <unistd.h>
 
+//数字をまとめる構造体
 typedef struct s_list
 {
-	struct num_list	 	*stuck_a;
-	struct num_list		*stuck_b;
+	struct num_list	 	*num;
 	struct bench_list *bench;
 	struct s_list	*next;
 }					t_list;
 
-
+//各数字に意味づけをする構造体
 typedef struct num_list
 {
-	int				*number;
-	int 			*level;
-	struct s_list	*next;
+	int				*number;//記入された数字
+	int 			*level;//前からの順番（必要かわからない）
+	struct num_list	 	*stuck_a;//基本aかbにいる（アドレスを指す）
+	struct num_list		*stuck_b;
+	struct s_list	*next; //記入された順
 }					num;
 
-
+//benchと打った時に出てくる詳細をまとめる構造体
 typedef struct bench_list
 {
-	int *disorder; //
-	char *strategy;
-	int *total_ops;
-	struct move_list *move;
+	int *disorder; //記入された数字の混沌度　%はft_printfで出力するのかな？？
+	char *strategy;//使用したアルゴリズム
+	int *total_ops;//行動した回数の総数
+	struct move_list *move;//各コマンドの詳細
 
 }	bench;
 
